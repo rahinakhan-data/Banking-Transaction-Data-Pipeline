ec4bec2e117f3bc070a7092bc4bb47c1f5d30742
rahinakhan-data <rahinakhan2k22@gmail.com>
2026-08-13 16:02:01 +0530

    initialize centralized logging module for tracking pipeline pipeline milestones

--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,13 @@
 import os
 import logging
+from config import LOGS_DIR
 
-parent_dir = os.getcwd()
-log_dir = os.path.join(parent_dir, 'logs')
-os.makedirs(log_dir, exist_ok=True)
-log_file_path = os.path.join(log_dir, 'etl.log')
+log_file_path = os.path.join(LOGS_DIR, 'etl.log')
 
 def etl_logger():
 
     # set the format of log : Time - Status - Message
-    log_format = " %(asctime)s - %(levelname)s - %(message)s"
+    log_format = "[%(asctime)s] - %(levelname)s - %(message)s"
     date_format = "%Y-%m-%d %H:%M:%S"
 
     logging.basicConfig(
@@ -18,6 +16,6 @@ def etl_logger():
         datefmt = date_format,
         handlers = [logging.FileHandler(log_file_path), logging.StreamHandler()]
     )
-    return logging.getLogger("ETL _LOgger")
+    return logging.getLogger()
 
 logger = etl_logger()
