85d216f5de08225a82a3f8b2f1eb73624b96249e
rahinakhan-data <rahinakhan2k22@gmail.com>
2026-09-01 23:34:31 +0530

    update paths and environment variables in config.py

--- src/config.py
+++ src/config.py
@@ -44,11 +44,18 @@ FRAUD_CSV_PATH = os.path.join(FRAUD_DIR, 'fraud_transactions.csv')
 # ===================================================================
 # 4. SECURE DATABASE CONFIGURATION (Task 15 & Task 16 Compliant)
 # ===================================================================
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+# DB_USER = os.getenv('DB_USER')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_HOST = os.getenv('DB_HOST')
+# DB_PORT = os.getenv('DB_PORT')
+# DB_NAME = os.getenv('DB_NAME')
+
+DB_USER = os.getenv('DB_USER', 'airflow')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'airflow')
+DB_HOST = os.getenv('DB_HOST', 'postgres')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'airflow')
+
 
 # Connection string syntax creation for SQLAlchemy Engine
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
