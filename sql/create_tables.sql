91d07c2566686c84818f6648879cc782e7f592b0
rahinakhan-data <rahinakhan2k22@gmail.com>
2026-09-14 18:34:05 +0530

    update tables schema and optimization indexes

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,3 @@
-
-
 -- create table transactions_staging inside staging schema 
 CREATE TABLE staging.transactions_staging(
 	transaction_id VARCHAR(50),
@@ -16,7 +14,6 @@ CREATE TABLE staging.transactions_staging(
 );
 SELECT * FROM staging.transactions_staging
 SELECT COUNT(*) FROM staging.transactions_staging
-
 
 -- CREATE DIMENSION TABLES  
 -- i) Customer Dimension
@@ -80,10 +77,48 @@ CREATE TABLE IF NOT EXISTS audit.etl_run_log(
 
 SELECT * FROM audit.etl_run_log
 
+-- ===================================================================
+-- Week 5 Task 3 – Create ETL Control Table
+-- ===================================================================
+CREATE TABLE IF NOT EXISTS audit.pipeline_control (
+    pipeline_name VARCHAR(100) PRIMARY KEY,
+    last_successful_run TIMESTAMP,
+    last_processed_timestamp TIMESTAMP,
+    last_run_status VARCHAR(20),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+INSERT INTO audit.pipeline_control (pipeline_name, last_run_status, last_successful_run)
+VALUES ('banking_dw_load_pipeline', 'INIT', '1970-01-01 00:00:00')
+ON CONFLICT (pipeline_name) DO NOTHING;
 
+SELECT * FROM audit.pipeline_control;
 
+-- ===================================================================
+-- Week 5 Task 9 – Database Indexing Implementation Script
+-- ===================================================================
 
+-- 1. Index for Quick Search & Duplicate Detection (transaction_id)
+CREATE INDEX IF NOT EXISTS idx_fact_transaction_id 
+ON warehouse.fact_transactions (transaction_id);
 
+-- 2. Indexes for High-Velocity Relational Dimension Tables Joins
+CREATE INDEX IF NOT EXISTS idx_fact_customer_key 
+ON warehouse.fact_transactions (customer_key);
 
+CREATE INDEX IF NOT EXISTS idx_fact_branch_key 
+ON warehouse.fact_transactions (branch_key);
 
+-- 3. Index for Analytical Warehouse Time-Series & Operational Reporting
+CREATE INDEX IF NOT EXISTS idx_fact_date_key 
+ON warehouse.fact_transactions (date_key);
+
+-- 4. Staging Area Optimization Index
+
+CREATE INDEX IF NOT EXISTS idx_stg_txn_datetime 
+ON staging.transactions_staging (transaction_datetime);
+
+-- ===================================================================
+-- Verification Dashboard (Run this to monitor execution status plans)
+-- ===================================================================
+-- EXPLAIN ANALYZE SELECT * FROM warehouse.fact_transactions WHERE transaction_id = 'WTX1062467';
